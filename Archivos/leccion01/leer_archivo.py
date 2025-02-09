e263fe4a7d3498a61cd9a4317cf4c38750049a9b
unknown <mejiaperezjahazielisrael@gmail.com>
2025-02-09 09:45:23 -0600

    carpeta archivos

---
+++ Archivos/leccion01/leer_archivo.py
@@ -0,0 +1,32 @@
+# # archivo= open("c:\\cursos\\Python\\Archivos\\leccion01\\prueba.txt","r",encoding="utf8")
+#
+# archivo= open("prueba.txt", "r", encoding="utf8")
+# # print(archivo.read())
+#
+# #leer algunos caracteres:
+# # print(archivo.read(10))
+#
+# #leer lineas completas:
+# # print(archivo.readline())
+# # print(archivo.readline())
+# # print(archivo.readline())
+#
+# # iterar el archivo
+# # for linea in archivo:
+# #     print(linea)
+#
+# # leer lineas
+# # print(archivo.readlines())
+#
+# # acceder a una linea de la lista
+# # print(archivo.readlines()[1])
+#
+# # abrimos un archivo
+# # a - anexar informacion
+# archivo2 = open("copia.txt", "a", encoding= "utf8")
+# archivo2.write(archivo.read())
+#
+# archivo.close()
+# archivo2.close()
+#
+# print("se ha terminado el proceso de leer y copiar archivos ")
